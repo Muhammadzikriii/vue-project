638fa0f562550a19825dabd8d92f86260f3845f4
Muhammadzikriii <akunpubgzikri15@gmail.com>
2024-05-03 19:25:28 +0700

    first commit

---
+++ src/components/MyPortfolio.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <h3 class="text-center mt-3 mb-3">My Portfolio</h3>
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <img src="../assets/gambar/le.jpeg" class="card-img-top" alt="..." />
+      <div class="col">
+        <div class="card shadow">
+          <div class="card-body">
+            <p class="card-text">
+              Kemampuan saya dalam belajar Basis Data cukup meyakinkan, dulu saya bisa tanpa harus mencontek ke orang lain untuk belajar
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <div class="card-body">
+            <p class="card-text">
+              Skill saya untuk saat ini belum bisa untuk ditentukan, bisa angin anginan, bisa juga jago tiba tiba
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <div class="card-body">
+            <p class="card-text">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <div class="card-body">
+            <p class="card-text">
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center mt-3">
+      <a href="https://www.instagram.com/muhammaadzikrii_" class="btn btn-primary custom-btn" target="_blank">instagram</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap");
+h3 {
+  font-family: "Playfair Display", serif;
+  font-size: 35px;
+  padding: 5px;
+  border-bottom: 2px solid black;
+}
+.card {
+  border: 1px solid black;
+}
+
+/* Custom Button Style */
+.custom-btn {
+  font-family: "Playfair Display", serif;
+  font-size: 18px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #ffffff;
+  border: none;
+  transition: all 0.3s ease;
+}
+
+.custom-btn:hover {
+  background-color: #0056b3;
+}
+
+</style>
